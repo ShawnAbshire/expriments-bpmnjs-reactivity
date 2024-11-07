f711bfccbba6272fb55fefc66d454ffbb2894420
Shawn <abshire.shawn@gmail.com>
2024-11-06 22:48:25 -0600

    Initial bpmnjs setup

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './index.css';
+import 'bpmn-js/dist/assets/diagram-js.css'
+import 'bpmn-js/dist/assets/bpmn-font/css/bpmn.css'
+
+createApp(App).mount('#app')
